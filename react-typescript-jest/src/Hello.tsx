05e6502fa1e3e24ed7b27b85acde9c02584edfc9
Wenjie Yang <wenjie.yang@ihandysoft.com>
2018-06-07 19:59:14 +0800

    test

--- react-typescript-jest/src/Hello.tsx
+++ react-typescript-jest/src/Hello.tsx
@@ -5,10 +5,24 @@ interface Props {
     age: number;
 }
 
-class Hello extends React.Component<Props> {
+class Hello extends React.Component<Props, {count: number}> {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+
+    handleClick = (e) => {
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
+
     render() {
         return (
-            <div>{`${this.props.name}: ${this.props.age}`}</div>
+            <div onClick={ this.handleClick }>{`${this.props.name}: ${this.props.age}`}</div>
         )
     }
 }
